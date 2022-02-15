c96fa87d50510e674bb4d284b77151658651f6c2
jonathanpascoar <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2022-02-15 14:39:13 +0000

    First half completed

---
+++ turingMachine.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+# Created by: Jonathan Pasco-Arnone
+# Created on: February 2022
+# This is the turing machine
+
+def output(numberList):
+    # Prints all the digits
+    for number in numberList:
+        print(str(number), end = '')
+
+# Inserts an inputted number in the inputted index
+def insert(numberList, index, insertingNumber):
+    numberList.insert(index, insertingNumber)
+    return numberList
+
+# Erases the current index
+def erase(numberList, index):
+    numberList.pop(index)
+    return numberList
+
+# Moves the tape to the right once
+def moveRight(index):
+    index = index - 1
+    return index
+
+# Moves the tape to the left once
+def moveLeft(index):
+    index = index + 1
+    return index
+
+# all the commands
+def main():
+    # This function holds the main turing machine
+    numberList = [1,0,1,1]
+    # makes the index the same as the last digit in 
+    index = len(numberList) - 1
+    
+    print("11 in binary is ", end = '')
+    output(numberList)
+
+    # The rules that will repeat over and over
+    while True:
+
+        # If the index is one to the left out of bounds,
+        # then set the index to 0 and place a new value
+        # there to make it in bounds.
+        if index == -1:
+            index = 0
+            numberList = insert(numberList, index, "B")
+
+        # If the location is a 0
+        if numberList[index] == 0:
+            numberList = erase(numberList, index)
+            numberList = insert(numberList, index, 1)
+
+            # Moves tape left until at the furthest number
+            while index < (len(numberList) - 1):
+                index = moveLeft(index)
+
+        # If the location is a 1
+        elif numberList[index] == 1:
+            numberList = erase(numberList, index)
+            numberList = insert(numberList, index, 0)
+            index = moveRight(index)
+
+        # If the location is blank
+        else:
+            numberList = erase(numberList, index)
+            numberList = insert(numberList, index, 1)
+            break
+
+    print("\n\nChanged to 16 in binary which is ", end="")
+    output(numberList)
+    
+    print("\n\nDone.")
+
+if __name__ == "__main__":
+    main()
